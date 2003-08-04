ea341bd455eb946ef4e32a9713349e187bec8595
Johannes Berg <johannes@sipsolutions.net>
2003-08-04 19:11:30 +0000

    bugfix with caching
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@156 e0417a13-a44b-4b77-80e5-317e04f1597d

--- source/component/PasDoc.pas
+++ source/component/PasDoc.pas
@@ -385,10 +385,12 @@ begin
   if FSourceFileNames.IsEmpty then begin
     DoError('No Source Files have been specified.', [], 1);
   end;
-  CacheDir := IncludeTrailingPathDelimiter(CacheDir);
-  if not DirectoryExists(CacheDir) then begin
-    if not CreateDir(CacheDir) then begin
-      DoError('Cache directory does not exist and could not be created', [], 1);
+  if (CacheDir <> '') then begin
+    CacheDir := IncludeTrailingPathDelimiter(CacheDir);
+    if not DirectoryExists(CacheDir) then begin
+      if not CreateDir(CacheDir) then begin
+        DoError('Cache directory does not exist and could not be created', [], 1);
+      end;
     end;
   end;
 
