bdbf2408409387dfea4c70c9aa4215d2464db017
Carl Eric Codere <ccodere@ieee.org>
2004-04-20 02:00:16 +0000

    + compiling updates when error
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@223 e0417a13-a44b-4b77-80e5-317e04f1597d

--- source/compile.bat
+++ source/compile.bat
@@ -1,9 +1,14 @@
 ppc386 -Fucomponent -Fuconsole -FuOptionParser -S2 -CR -Cr -Sh .\console\Pasdoc_console.dpr
+if ERRORLEVEL 1 goto error
 cd console
 move pasdoc_console.exe ..\pasdoc.exe
 cd..
 del /s *.ow
 del /s *.ppw
+goto end
+:error
+Echo Error!
+:end
 
 
 
