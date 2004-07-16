f48f74ed8cef3f16132d1665c497d0859cd83642
Johannes Berg <johannes@sipsolutions.net>
2004-07-16 16:39:23 +0000

    fix script to not hardcode ppc386 path
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@236 e0417a13-a44b-4b77-80e5-317e04f1597d

--- source/build-fpc.sh
+++ source/build-fpc.sh
@@ -2,6 +2,6 @@
 
 #./prepare-fpc.sh
 
-/usr/local/bin/ppc386 -Mobjfpc console/PasDoc_Console.dpr -Fucomponent -FuOptionParser -Ficomponent -Sh \
+ppc386 -Mobjfpc console/PasDoc_Console.dpr -Fucomponent -FuOptionParser -Ficomponent -Sh \
   -Cr -g -gl -gg -Ci -Co -Ct -CR\
   $@
