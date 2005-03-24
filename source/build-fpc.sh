b0f3a49e55f3f2ebef66a49899de855b00f79a46
Johannes Berg <johannes@sipsolutions.net>
2005-03-24 10:14:43 +0000

    use 'fpc' instead of arch-dependent 'ppc386'
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@262 e0417a13-a44b-4b77-80e5-317e04f1597d

--- source/build-fpc.sh
+++ source/build-fpc.sh
@@ -2,6 +2,6 @@
 
 #./prepare-fpc.sh
 
-ppc386 -Mobjfpc console/PasDoc_Console.dpr -Fucomponent -FuOptionParser -Ficomponent -Sh \
+fpc -Mobjfpc console/PasDoc_Console.dpr -Fucomponent -FuOptionParser -Ficomponent -Sh \
   -Cr -g -gl -gg -Ci -Co -Ct -CR\
   $@
