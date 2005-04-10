fa151140198e15dcdeeb035917a3dd9b9e67f94b
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-04-10 22:25:31 +0000

    * warning_abstract_twice fixed
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@308 e0417a13-a44b-4b77-80e5-317e04f1597d

---
+++ tests/warning_abstract_twice.pas
@@ -0,0 +1,15 @@
+{ @abstract(First abstract)
+
+  @abstract(Second abstract)
+
+  Currently pasdoc ignores "First abstract" but doesn't warn about it.
+  Should warn "You used @@abstract twice in description of item ..."
+  or something like that. }
+
+unit warning_abstract_twice;
+
+interface
+
+implementation
+
+end.
