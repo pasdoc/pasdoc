a1252a1e0144675be9bae620d0c55f043c59f740
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-04-10 22:05:54 +0000

    * warning_abstract_termination fixed
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@307 e0417a13-a44b-4b77-80e5-317e04f1597d

--- tests/warning_abstract_termination.pas
+++ tests/warning_abstract_termination.pas
@@ -2,10 +2,7 @@
   unterminated.  PasDoc should either terminate the tag itself, give a warning,
   or both)
 
-  Submitted in thread "Pasdoc tests" 2004-04-10 on pasdoc-main.
-  The cause and the cure of this bug are known, 
-  it will be fixed after pasdoc's release
-  on sourceforge that should happen today (2004-04-10). }
+  Submitted in thread "Pasdoc tests" 2004-04-10 on pasdoc-main. }
 unit AbstractTermination;
 
 interface
