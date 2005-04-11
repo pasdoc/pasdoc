315ac8e7fb0b45d97356605a5e43ed1e91955ed9
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-04-11 14:11:50 +0000

    + Added
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@313 e0417a13-a44b-4b77-80e5-317e04f1597d

---
+++ tests/ok_complicated_record.pas
@@ -0,0 +1,50 @@
+// Submitted in thread "Pasdoc tests" on pasdoc-main on 2005-04-11
+// by Richard B Winston.
+// pasdoc passes it, but the test checks many important things
+// (line glueing single-line comments by pasdoc, record with case etc.)
+// so it's worth adding it to test cases.
+//
+// @abstract( This unit is used for converting to and from the BigEndian format.
+// See http://community.borland.com/article/0,1410,28964,00.html.)
+
+unit ComplicatedRecord;
+
+interface
+
+type
+  //enumeration used in variant record
+  BytePos = (EndVal, ByteVal);
+
+  // @name is a pointer to a @link(TDoubleEndianCnvRec).
+  PDoubleEndianCnvRec = ^TDoubleEndianCnvRec;
+  { @abstract(@name is used in @link(ConvertDouble) to convert
+   a double to or from the BigEndian format.)
+   @longcode(#
+  TDoubleEndianCnvRec = packed record
+    case BytePos of
+      EndVal: (EndianVal: double);
+      ByteVal: (Bytes: array[0..SizeOf(double) - 1] of byte);
+  end;
+   #)
+   }
+  TDoubleEndianCnvRec = packed record
+    case BytePos of
+      //The value we are trying to convert
+      EndVal: (EndianVal: double);
+      //Overlapping bytes of the double
+      ByteVal: (Bytes: array[0..SizeOf(double) - 1] of byte);
+  end;
+
+// @abstract(@name copies @link(TDoubleEndianCnvRec.Bytes)
+// in reverse order from Source^ to Dest^.)
+// @name is used in @link(ConvertDouble).
+procedure SwapDoubleBytes(Dest, Source: PDoubleEndianCnvRec);
+
+// @abstract(@name converts Value to or from the BigEndian format.)
+// @param(Value is the value to be converted.)
+// @returns(Value after being converted to or from the BigEndian format.)
+function ConvertDouble(const Value: double): double;
+
+implementation
+
+end.
---
+++ tests/todo/ok_paragraph_in_single_line_comment.pas
@@ -0,0 +1,24 @@
+// This is the 1st paragraph.
+//
+// This is the 2nd paragraph.
+//
+// This is the 3rd paragraph.
+//
+// pasdoc should create paragraphs when glueing single-line comments to
+// a description, but it doesn't for now.
+unit ok_paragraph_in_single_line_comment;
+
+interface
+
+{ This is the 1st paragraph.
+
+  This is the 2nd paragraph.
+
+  This is the 3rd paragraph.
+
+  Here paragraphs are correct. }
+procedure Foo;
+
+implementation
+
+end.
