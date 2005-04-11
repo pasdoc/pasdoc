3bd0bcea70d47061579ebc935f6fb489e2e317f7
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-04-11 21:45:07 +0000

    * autodoc/Makefile automatically sets group of uploaded files to pasdoc   and makes them writeable by the group.   Will be useful also for tests/scripts/upload_correct_tests_output.sh
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@318 e0417a13-a44b-4b77-80e5-317e04f1597d

---
+++ tests/scripts/ssh_chmod_writeable_by_pasdoc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eu
+
+# This is small script used by upload_correct_tests_output.sh
+# source/autodoc/Makefile.
+#
+# $1 is your username on sourceforge,
+# $2 is directory on sourcefore (usually you will start this with
+# /home/groups/p/pa/pasdoc/...).
+#
+# This script logs to sourceforge using given username
+# (you will be asked for password unless you set up ssh keys)
+# and executes
+#   chgrp -R pasdoc *
+#   chmod -R g+w *
+# This means that every file in given directory is made writeable
+# by pasdoc group, which means pasdoc developers.
+
+SF_USERNAME="$1"
+SF_PATH="$2"
+shift 2
+
+ssh -l "$SF_USERNAME" shell.sourceforge.net <<EOF
+  cd "$SF_PATH"
+  chgrp -R pasdoc *
+  chmod -R g+w *
+EOF
