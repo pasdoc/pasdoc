f49e9d12e5385bb4ec76baf560daa25279049abb
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-04-26 12:27:09 +0000

    + warning_no_params bug
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@349 e0417a13-a44b-4b77-80e5-317e04f1597d

---
+++ tests/todo/warning_no_params.pas
@@ -0,0 +1,28 @@
+unit warning_no_params;
+
+interface
+
+{ Parens are missing here, generated docs will not be as expected.
+  PasDoc should warn about this.
+
+  @returns something }
+function Foo1(A: Integer): Integer;
+
+{ Parens are missing here, generated docs will not be as expected.
+  PasDoc should warn about this.
+
+  @param A means something }
+function Foo2(A: Integer): Integer;
+
+type
+  EFoo = class(Exception);
+
+{ Parens are missing here, generated docs will not be as expected.
+  PasDoc should warn about this.
+
+  @raises EFoo when something is bad }
+function Foo3(A: Integer): Integer;
+
+implementation
+
+end.
