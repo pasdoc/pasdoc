6e6463bc7ff7993d75965e027c345a74f3fe1d42
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-05-07 14:57:16 +0000

    * Test improved
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@393 e0417a13-a44b-4b77-80e5-317e04f1597d

--- tests/todo/ok_platform_hints.pas
+++ tests/todo/ok_platform_hints.pas
@@ -1,5 +1,5 @@
 { @abstract(This unit tests parsing platform, library and deprecated
-  directives by pasdoc)
+  directives (called collectively "hint directives") by pasdoc.)
 
   I'm doing this testcase in order to fix bug submitted to tracker:
   [ 1196073 ] some modifiers are not parsed
@@ -12,10 +12,38 @@
   class and structure declarations, field declarations within classes or
   records, procedure, function and method declarations, and unit declarations."
 
-  But I'm unable to figure out how to specify them for procedures,
-  functions, normal types and methods. Anyone can improve this testcase,
-  please ? Or point me to some precide documentation how such directives
-  are parsed by Delphi ?
+  Looking below you can see that the way how these hints are parsed
+  by Delphi is pretty much stupid and non-consequent (sorry all Delphi
+  lovers, but this is really a mess) :
+
+  1. Between "unit UnitName" and hints you *mustn't* put any semicolon,
+     and you *mustn't* put any semicolons between hints.
+     Same thing for CIOs (Classes / Interfaces / Objects / Records).
+     Same thing for CIOs fields.
+     Same thing for variables.
+     Same thing for constants.
+
+  2. Between "procedure/function Name (...)" and hints you *must*
+     put a semicolon, and semicolons between hints are allowed but
+     not required. It seems that you can't specify "library" directive
+     for procedures/functions -- why ? Probably because "library"
+     is a keyword and Borland was unable to correctly modify it's compiler
+     to parse such thing.
+
+  3. Between method and hints you *must* put a semicolon,
+     and semicolon between hints is *required*.
+     You can specify "library" directive for methods.
+
+  I'm unable to figure out how to specify these hints for normal
+  (non-structural) types. If anyone can
+  - tell me how to specify hint directives for non-structural types or
+  - explain why parsing these directives is so non-consequent in Delphi or
+  - point me to some precise documentation by Borland specifying grammar
+    rules with these directives
+  ... then please send email about this to pasdoc-main mailing list
+  (or directly to me, Michalis Kamburelis, <kambi@users.sourceforge.net>,
+  if your comments about this do not really concern pasdoc).
+  I will be grateful.
 }
 
 {$ifdef FPC}
@@ -26,23 +54,25 @@ unit ok_platform_hints platform library deprecated;
 
 interface
 
-procedure TestProcPlatform {platform};
+procedure TestProcPlatform; platform;
 
-procedure TestProcLibrary {library};
+procedure TestProcLibrary; {library;}
 
-procedure TestProcDeprecated {deprecated};
+procedure TestProcDeprecated; deprecated;
 
-procedure TestProcCombined(SomeParams: Integer)
-  {library deprecated platform};
+procedure TestProcCombined(SomeParams: Integer);
+  {library }deprecated platform;
 
-function TestFuncPlatform: Integer {platform};
+function TestFuncPlatform: Integer; platform;
 
-function TestFuncLibrary: Integer {library};
+function TestFuncLibrary: Integer; {library;}
 
-function TestFuncDeprecated: Integer {deprecated};
+function TestFuncDeprecated: Integer; deprecated;
 
-function TestFuncCombined(SomeParams: Integer): Integer
-  {library deprecated platform};
+function TestFuncCombined(SomeParams: Integer): Integer;
+  {library }
+  deprecated; { <- this semicolon is allowed but is optional }
+  platform;
 
 type
   TTestTypePlatform = Integer {platform};
@@ -52,8 +82,15 @@ type
 
   TTestClassDeprecated = class
     TestFieldPlatform: Integer platform;
-    procedure TestMethodLibrary {library};
-  end deprecated;
+    TestFieldLibrary: Integer library;
+    TestFieldDeprecated: Integer deprecated;
+    TestFieldCombined: Integer library deprecated platform;
+    
+    procedure TestMethodLibrary; library;
+    procedure TestMethodPlatform; platform;
+    procedure TestMethodDeprecated; deprecated;
+    procedure TestMethodCombined; library; deprecated; platform;
+  end deprecated library;
 
   TTestRecordDeprecated = record
     TestFieldPlatform: Integer platform;
@@ -106,6 +143,18 @@ begin
 end;
 
 procedure TTestClassDeprecated.TestMethodLibrary;
+begin
+end;
+
+procedure TTestClassDeprecated.TestMethodPlatform;
+begin
+end;
+
+procedure TTestClassDeprecated.TestMethodDeprecated;
+begin
+end;
+
+procedure TTestClassDeprecated.TestMethodCombined;
 begin
 end;
 
