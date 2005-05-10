dd591acb496eff36cfa9e7b7252d4315ee7094d2
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-05-10 16:08:40 +0000

    * Test recursive tags inside @member and @value
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@410 e0417a13-a44b-4b77-80e5-317e04f1597d

--- tests/ok_value_member_tags.pas
+++ tests/ok_value_member_tags.pas
@@ -6,7 +6,9 @@ type
   { @member MyField Description of MyField here.
     @member(MyMethod Description of MyMethod here, 
       using parenthesis.)
-    @member MyProperty Description of MyProperty here. }
+    @member(MyProperty Description of MyProperty here,
+      with some recursive tags inside: @code(Some code with
+      a link to @link(TMyRecord)).) }
   TMyClass = class
     MyField: Integer;
     procedure MyMethod;
@@ -19,7 +21,7 @@ type
   end;
   
   { @value meOne Description of meOne follows.
-    @value meThree Description of meThree follows. }
+    @value meThree Description of meThree, with some link: @link(TMyClass.MyField). }
   TMyEnum = (meOne, meTwo, meThree);
   
 implementation
