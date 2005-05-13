0655e1ccc08b050a76af949268b38d1f5c23ef00
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-05-13 04:48:16 +0000

    * test tests/todo/warning_incorrect_tag_nesting.pas is now fixed,   it's also improved a little, moved to tests/warning_incorrect_tag_nesting.pas
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@426 e0417a13-a44b-4b77-80e5-317e04f1597d

---
+++ tests/warning_incorrect_tag_nesting.pas
@@ -0,0 +1,30 @@
+unit warning_incorrect_tag_nesting;
+
+interface
+
+type
+  EFoo = class(Exception);
+
+{ Now there is control inside PasDoc what tags
+  can be embedded inside what.
+
+  pasdoc should display appropriate warnings on following input:
+
+  @code(This is a code @code(inside a code ???).)
+  
+  @code(This is a code @raises(EFoo with a raises inside ???).)
+
+  @raises(@raises(EFoo What are you doing, using raises inside @@raises ?!?))
+  
+  @created(2005-05-13 @unknowntag, this will cause a warning 
+    about unknown_tag.)
+  
+  @created(2005-05-13 
+    @code(this will cause a warning) that no known tag 
+    may be used within @@created.)
+}
+procedure TestMe;
+
+implementation
+
+end.
