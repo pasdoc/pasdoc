c3f65353f573d4a8195fe6cc12668490367432fd
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-05-17 07:20:24 +0000

    + file_to_pascal_data.dpr, file_to_pascal_string.dpr helper programs   (used by PasDoc_Tipue, will also be used to convert visibility images to inc)
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@457 e0417a13-a44b-4b77-80e5-317e04f1597d

---
+++ source/tools/file_to_pascal_data.dpr
@@ -0,0 +1,53 @@
+{ Reads file $1 and writes Pascal source file $2,
+  such that $2 file contains type and value of Pascal constant, i.e.
+    array[0 .. Xxx]of Byte = ( ... )
+  that has the contents of file $1.
+
+  File is treated as binary file, so file contents are reflected exactly,
+  byte by byte, in $2.
+
+  Useful if you want to "embed" some binary file inside compiled
+  Pascal program. }
+
+program file_to_pascal_data;
+
+{$apptype CONSOLE}
+{$ifdef FPC} {$mode objfpc} {$endif}
+
+uses SysUtils, Classes;
+
+var
+  Src: TFileStream;
+  Dest: TextFile;
+  SrcFileName, DestFileName: string;
+  B: Byte;
+  i: Integer;
+begin
+  SrcFileName := ParamStr(1);
+  DestFileName := ParamStr(2);
+
+  Src := TFileStream.Create(SrcFileName, fmOpenRead);
+  try
+    Assign(Dest, DestFileName);
+    Rewrite(Dest);
+    try
+      Writeln(Dest, '{ -*- buffer-read-only: t -*- }');
+      Writeln(Dest, '{ DON''T EDIT -- this file was automatically generated from "'
+        + SrcFileName + '" }');
+
+      Writeln(Dest, 'array [0 .. ', Src.Size - 1, '] of Byte = (');
+
+      for i := 0 to Src.Size - 2 do
+      begin
+        Src.ReadBuffer(B, 1);
+        Write(Dest, '$', IntToHex(B, 2), ', ');
+        if ((i + 1) mod 15) = 0 then
+          Writeln(Dest);
+      end;
+
+      Src.ReadBuffer(B, 1);
+      Write(Dest, '$', IntToHex(B, 2));
+      Writeln(Dest, ')');
+    finally CloseFile(Dest) end;
+  finally Src.Free end;
+end.
---
+++ source/tools/file_to_pascal_string.dpr
@@ -0,0 +1,45 @@
+{ Reads text file $1 and writes Pascal source file $2,
+  such that $2 file contains Pascal string that has the contents
+  of file $1. 
+  
+  Useful if you want to "embed" some text file inside compiled
+  Pascal program.
+
+  Note it treats $1 as text file, so difference between various line endings
+  is lost in $2 (line endings are always encoded using LineEnding constant). }
+
+program file_to_pascal_string;
+
+{$apptype CONSOLE}
+{$ifdef FPC} {$mode objfpc} {$endif}
+
+uses SysUtils;
+
+var
+  Src, Dest: TextFile;
+  SrcFileName, DestFileName, S: string;
+begin
+  SrcFileName := ParamStr(1);
+  DestFileName := ParamStr(2);
+
+  Assign(Src, SrcFileName);
+  Reset(Src);
+  try
+    Assign(Dest, DestFileName);
+    Rewrite(Dest);
+    try
+      Writeln(Dest, '{ -*- buffer-read-only: t -*- }');
+      Writeln(Dest, '{ DON''T EDIT -- this file was automatically generated from "'
+        + SrcFileName + '" }');
+
+      while not Eof(Src) do
+      begin
+        Readln(Src, S);
+        S := StringReplace(S, '''', '''''', [rfReplaceAll]);
+        Writeln(Dest, '''' + S + ''' + LineEnding + ');
+      end;
+      
+      Writeln(Dest, '''''');
+    finally CloseFile(Dest) end;
+  finally CloseFile(Src) end;
+end.
