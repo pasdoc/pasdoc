14c49a16b018262bfc23a6c994be6827b60413b6
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-05-27 23:42:30 +0000

    * Test improved
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@522 e0417a13-a44b-4b77-80e5-317e04f1597d

--- tests/ok_multiple_vars.pas
+++ tests/ok_multiple_vars.pas
@@ -6,14 +6,14 @@ type
   TMyClass = class
     { Docs for A and B }
     A, B: Integer;
-    { Docs for A and B }
+    { Docs for C and D }
     C, D: procedure(A: Integer): boolean;  
   end;
 
 var 
   { Docs for A and B }
   A, B: Integer;
-  { Docs for A and B }
+  { Docs for C and D }
   C, D: procedure(A: Integer): boolean;
 
 implementation
