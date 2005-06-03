fbeeeab5b2f7cc31385b3a1c18ce593fa4540324
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-06-03 21:47:59 +0000

    + Test of --latex-head
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@564 e0417a13-a44b-4b77-80e5-317e04f1597d

---
+++ tests/ok_latex_head.pas
@@ -0,0 +1,14 @@
+{ Unit testing --latex-head option. Very long word: 
+  unitfortestingoflatexheadoptionveryveryveryveryverylongword.
+
+  Another very long word, this time it should be broken because of \hyphenation:
+  anotherverylongwordthistimeitshouldbebrokenbecauseofhyphenation.
+}
+
+unit ok_latex_head;
+
+interface
+
+implementation
+
+end.
--- tests/scripts/mk_tests.sh
+++ tests/scripts/mk_tests.sh
@@ -70,7 +70,8 @@ mk_test "$FORMAT"/ "$SORT_OLD" \
   -iname 'ok_introduction_conclusion.pas' -or \
   -iname 'ok_property_decl.pas' -or \
   -iname 'ok_multiple_vars.pas' -or \
-  -iname 'ok_class_function.pas' \
+  -iname 'ok_class_function.pas' -or \
+  -iname 'ok_latex_head.pas' \
   ')' | sort`
 
 # Make a specialized test of some units that need special
@@ -92,3 +93,4 @@ mk_special_test ok_introduction_conclusion ok_introduction_conclusion.pas --intr
 mk_special_test ok_property_decl ok_property_decl.pas
 mk_special_test ok_multiple_vars ok_multiple_vars.pas
 mk_special_test ok_class_function ok_class_function.pas
+mk_special_test ok_latex_head --latex-head=ok_latex_head.tex ok_latex_head.pas
