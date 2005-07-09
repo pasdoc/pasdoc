a5bc568695f8173735f67e18ba5f15e23eb032a7
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-07-09 18:04:26 +0000

    * Updated version number to 0.9.0, this commit will also update PASDOC_DATE
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@680 e0417a13-a44b-4b77-80e5-317e04f1597d

--- source/component/PasDoc.pas
+++ source/component/PasDoc.pas
@@ -219,7 +219,7 @@ const
   { }
   PASDOC_DATE = '$Date$';
   { }
-  PASDOC_VERSION = '0.8.8.3';
+  PASDOC_VERSION = '0.9.0';
   { }
   PASDOC_NAME_AND_VERSION = PASDOC_NAME + ' ' + PASDOC_VERSION;
   { }
