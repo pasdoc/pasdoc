e62724179127a88d5016d5719d184167229dbd77
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-12-22 18:20:11 +0000

    * Better workaround for FPC 2.0.0 and 2.0.2 GetAppConfigDir under Unixes
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@896 e0417a13-a44b-4b77-80e5-317e04f1597d

--- source/gui/pasdocguisettings.pas
+++ source/gui/pasdocguisettings.pas
@@ -27,35 +27,79 @@ var
 implementation
 
 {$ifdef UNIX}
-function GetApplicationNameWithDot: string;
+
+{ Code below is copied from FPC 2.0.3 rtl/unix/sysutils.pp.
+
+  It has fixed GetAppConfigDir and GetAppConfigFile, so that
+  1. config file is stored inside hidden dir
+  2. moreover it follows base-dir spec
+  
+  Michalis send this patch to FPC and it was applied during FPC 2.0.3
+  development. So for FPC 2.0.0 and 2.0.2, we need the workaround below. }
+  
+Function GetHomeDir : String;
+
 begin
-  Result := '.pasdoc_gui';
+  Result:=GetEnvironmentVariable('HOME');
+  If (Result<>'') then
+    Result:=IncludeTrailingPathDelimiter(Result);
+end;
+  
+{ Follows base-dir spec, 
+  see [http://freedesktop.org/Standards/basedir-spec].
+  Always ends with PathDelim. }
+Function XdgConfigHome : String;
+begin
+  Result:=GetEnvironmentVariable('XDG_CONFIG_HOME');
+  if (Result='') then
+    Result:=GetHomeDir + '.config/' 
+  else
+    Result:=IncludeTrailingPathDelimiter(Result);
+end;
+
+Function GetAppConfigDir(Global : Boolean) : String;
+
+begin
+  If Global then
+    Result:=SysConfigDir
+  else
+    Result:=XdgConfigHome + ApplicationName;
+end;
+
+Function GetAppConfigFile(Global : Boolean; SubDir : Boolean) : String;
+
+begin
+  if Global then
+    begin
+    Result:=IncludeTrailingPathDelimiter(SysConfigDir);
+    if SubDir then
+      Result:=IncludeTrailingPathDelimiter(Result+ApplicationName);
+    Result:=Result+ApplicationName+ConfigExtension;
+    end
+  else
+    begin
+    if SubDir then
+      begin
+      Result:=IncludeTrailingPathDelimiter(GetAppConfigDir(False));
+      Result:=Result+ApplicationName+ConfigExtension;
+      end
+    else
+      begin
+      Result:=XdgConfigHome + ApplicationName + ConfigExtension;
+      end;
+    end;
 end;
 {$endif}
 
 var
   ConfigDir: string;
 initialization
-  {$ifdef UNIX}
-  { Workaround for FPC problem with GetAppConfigDir(false):
-    it returns
-      GetHomeDir + ApplicationName
-    while I believe it should return
-      GetHomeDir + '.' + ApplicationName }
-  OnGetApplicationName := @GetApplicationNameWithDot;
-  {$endif}
-
   { calculate ConfigDir }
   ConfigDir := GetAppConfigDir(false);
   if not ForceDirectories(ConfigDir) then
     raise Exception.CreateFmt('Cannot create directory for config file: "%s"',
       [ConfigDir]);
   ConfigDir := IncludeTrailingPathDelimiter(ConfigDir);
-
-  {$ifdef UNIX}
-  { Turn the workaround off }
-  OnGetApplicationName := nil;
-  {$endif}
 
   { initialize IniFile }
   IniFile := TIniFile.Create(ConfigDir + 'prefs.ini');
