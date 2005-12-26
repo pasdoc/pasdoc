c10f11da96ac2c306d2455c47ff540fee0ab0183
Michalis Kamburelis <michalis.kambi@gmail.com>
2005-12-26 01:05:25 +0000

    * Fixed autodoc (paragraph breaks within @italic not allowed)
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@913 e0417a13-a44b-4b77-80e5-317e04f1597d

--- source/component/Hashes.pas
+++ source/component/Hashes.pas
@@ -9,20 +9,21 @@
                  error checks are missing!)
 
   @italic(
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.)
+  
+  @italic(
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.)
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-  )
+  @italic(
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA)
 
   Thanks to:
   @unorderedList(
