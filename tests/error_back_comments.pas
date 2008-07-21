24c57f5517f269eb4deac30a73a9985631656f62
Hans-Peter Diettrich <drdiettrich1@aol.com>
2008-07-21 16:14:38 +0000

    Bad back-comment handling.
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@1043 e0417a13-a44b-4b77-80e5-317e04f1597d

---
+++ tests/error_back_comments.pas
@@ -0,0 +1,18 @@
+unit test;
+
+interface
+
+type
+  MyEnum = (
+    me1, //<this is me1
+    me2  //<this is me2
+  ); //<this is MyEnum - missing!
+
+var
+  v1, //<this is v1 - missing!
+  v2,
+  v3: integer; //<this is v2, v3
+
+implementation
+
+end.
