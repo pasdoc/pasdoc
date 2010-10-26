01782da1a4e6459d07a6f30074a15eb3b457825f
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-10-26 06:36:31 +0000

    Update really old comments and copyrights in pasdoc.dpr.
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@1208 e0417a13-a44b-4b77-80e5-317e04f1597d

--- source/console/pasdoc.dpr
+++ source/console/pasdoc.dpr
@@ -3,16 +3,19 @@
   PasDoc
 
   * generates documentation from comments in Pascal unit source code files
-  * command line program
-  * written in Delphi
-  * output formats Html and HtmlHelp
-  * try PasDoc on its own source code
+  * command line program (GUI version also available, see ../gui/,
+    as well as components for Lazarus and Delphi)
+  * written in ObjectPascal (can be compiled by FPC or Delphi)
+  * output formats Html, HtmlHelp, LaTeX
+  * try PasDoc on its own source code (see ../autodoc/)
 
   * distributed under the GNU General Public License (GPL)
 
   * copyright (C) 1998-2000 by Marco Schmidt
   * copyright (C) 2001-2003 by Ralf Junker <delphi@zeitungsjunge.de>
   * Copyright (C) 2003 by Johannes Berg <johannes@sipsolutions.de>
+  * Copyright 2005-2010 by Michalis Kamburelis, Richard B. Winston
+    and other contributors, see ../../ChangeLog file
 
   Hint:
 
