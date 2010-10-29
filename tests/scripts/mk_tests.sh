3de27bead597017ae05d7b87bddd869be1be34b0
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-10-29 22:22:00 +0000

    Simplify mk_test.sh a little, place every test in some subdirectory.
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@1240 e0417a13-a44b-4b77-80e5-317e04f1597d

--- tests/scripts/mk_tests.sh
+++ tests/scripts/mk_tests.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # This script creates tests output for particular pasdoc output format.
-# $1 is this format name. It's also the name of subdirectory where
+# $1 is the output format name. It's also the name of subdirectory where
 # tests output will be placed.
 #
 # This script is meant to be called only from Makefile in this directory.
@@ -25,13 +25,12 @@ run_echo ()
   set -e
 }
 
-# $1 is name of subdir (must end with /) where to put test output
-# (including PASDOC-OUTPUT file).
-# Rest of params are pasdoc's command-line:
-# special command-line parameters and unit files to process.
+# Run pasdoc with given command-line parameters.
+# First parameter is the name of subdirectory where we place test results
+# (including PASDOC-OUTPUT file), this is always inside "$FORMAT" subdirectory.
 mk_test ()
 {
-  OUTPUT_PATH="$1"
+  OUTPUT_PATH="$FORMAT"/"$1"/
   shift 1
 
   mkdir -p "$OUTPUT_PATH"
@@ -41,15 +40,6 @@ mk_test ()
   run_echo "$PASDOC_OUTPUT_FILENAME" \
     pasdoc --format "$FORMAT" --exclude-generator \
     --output="$OUTPUT_PATH" "$@"
-}
-
-# Like mk_test but $1 is name of subdir inside "$FORMAT" subdir.
-mk_special_test ()
-{
-  OUTPUT_PATH="$FORMAT"/"$1"/
-  shift 1
-
-  mk_test "$OUTPUT_PATH" "$@"
 }
 
 # parse params ----------------------------------------
@@ -72,9 +62,9 @@ shift 1
 #    is harder to grok for humans.
 #
 # Instead, usually you should add new test units as new calls to
-# `mk_special_test ...' lower in this script.
+# `mk_test ...' lower in this script.
 #
-mk_test "$FORMAT"/ "$SORT_OLD" \
+mk_test large_test "$SORT_OLD" \
   error_line_number.pas \
   ok_cdecl_external.pas \
   ok_complicated_record.pas \
@@ -109,99 +99,99 @@ mk_test "$FORMAT"/ "$SORT_OLD" \
 # so that it's easy to temporary switch some test on/off by
 # simply commenting / uncommenting that line.
 #
-mk_special_test ok_const_1st_comment_missing --marker=: "$SORT_OLD" ok_const_1st_comment_missing.pas
-mk_special_test ok_link_1_char --visible-members 'private,public,published' "$SORT_OLD" ok_link_1_char.pas
-mk_special_test ok_auto_abstract --auto-abstract "$SORT_OLD" ok_auto_abstract.pas
-mk_special_test warning_incorrect_tag_nesting "$SORT_OLD"  warning_incorrect_tag_nesting.pas
-mk_special_test ok_param_raises_returns_proctype "$SORT_OLD" ok_param_raises_returns_proctype.pas
-mk_special_test ok_no_sort '--sort=functions,non-record-fields,methods,properties' ok_no_sort.pas
-mk_special_test ok_sorting_all "$SORT_ALL" ok_sorting.pas
-mk_special_test ok_sorting_none --sort= ok_sorting.pas
-mk_special_test ok_introduction_conclusion ok_introduction_conclusion.pas --introduction=ok_introduction.txt --conclusion=ok_conclusion.txt
-mk_special_test ok_property_decl ok_property_decl.pas
-mk_special_test ok_multiple_vars ok_multiple_vars.pas
-mk_special_test ok_class_function ok_class_function.pas
-mk_special_test ok_latex_head --latex-head=ok_latex_head.tex ok_latex_head.pas
-mk_special_test ok_longcode_underscores ok_longcode_underscores.pas
-mk_special_test ok_longcode_comment ok_longcode_comment.pas
-mk_special_test ok_longcode_dash ok_longcode_dash.pas
-mk_special_test ok_longcode_special_chars ok_longcode_special_chars.pas
-mk_special_test error_introduction_twice_anchors_unit --introduction=error_introduction_twice_anchors.txt error_introduction_twice_anchors_unit.pas
-mk_special_test ok_longcode_float_hex ok_longcode_float_hex.pas
-mk_special_test ok_see_also ok_see_also.pas
-mk_special_test ok_implicit_visibility_public ok_implicit_visibility.pas
-mk_special_test ok_implicit_visibility_published --implicit-visibility=published ok_implicit_visibility.pas
-mk_special_test ok_implicit_visibility_implicit_yes --implicit-visibility=implicit --visible-members=public,implicit ok_implicit_visibility.pas
-mk_special_test ok_implicit_visibility_implicit_no  --implicit-visibility=implicit --visible-members=public          ok_implicit_visibility.pas
-mk_special_test ok_bold_italic ok_bold_italic.pas
-mk_special_test warning_inherited_test warning_inherited_test.pas
-mk_special_test ok_preformatted_test ok_preformatted_test.pas
-mk_special_test ok_dashes ok_dashes.pas
-mk_special_test ok_lists ok_lists.pas
-mk_special_test warning_lists warning_lists.pas
-mk_special_test ok_table ok_table.pas
-mk_special_test ok_table_nonlatex ok_table_nonlatex.pas
-mk_special_test warning_table warning_table.pas
-mk_special_test ok_macros ok_macros.pas
-mk_special_test error_macros error_macros.pas
-mk_special_test error_macros_recursive error_macros_recursive.pas
-mk_special_test ok_macros_off --no-macro ok_macros_off.pas
-mk_special_test ok_item_set_number ok_item_set_number.pas
-mk_special_test error_unexpected_eof error_unexpected_eof.pas
-mk_special_test error_unexpected_eof_2 error_unexpected_eof_2.pas
-mk_special_test error_expected_semicolon error_expected_semicolon.pas
-mk_special_test ok_record_case_parsing ok_record_case_parsing.pas
-mk_special_test ok_record_with_case ok_record_with_case.pas
-mk_special_test ok_multiple_fields ok_multiple_fields.pas
-mk_special_test ok_back_comment ok_back_comment.pas
-mk_special_test warning_back_comment warning_back_comment.pas
-mk_special_test ok_auto_link --auto-link ok_auto_link.pas
-mk_special_test ok_introduction_pre_link ok_introduction_pre_link_unit.pas --introduction=ok_introduction_pre_link.txt
-mk_special_test ok_table_of_contents ok_table_of_contents_unit.pas --introduction=ok_table_of_contents.txt
-mk_special_test warning_link_in_seealso warning_link_in_seealso.pas
-mk_special_test ok_caret_character ok_caret_character.pas
-mk_special_test ok_unit_uses_filename --write-uses-list ok_unit_uses_filename.pas
-mk_special_test ok_enum_explicit_assign ok_enum_explicit_assign.pas
-mk_special_test ok_if_directive ok_if_directive.pas
-mk_special_test ok_include_environment ok_include_environment.pas
-mk_special_test ok_enum_explicit_values ok_enum_explicit_values.pas
-mk_special_test ok_description_test --description ok_description_test.txt ok_description_test.pas
-mk_special_test ok_program --write-uses-list ok_program.pas
-mk_special_test ok_operator_test ok_operator_test.pas
-mk_special_test error_line_number_2 error_line_number_2.pas
-mk_special_test ok_dispid_method ok_dispid_method.pas
-mk_special_test ok_longcode_highlight ok_longcode_highlight.pas
-mk_special_test ok_non_matching_paren ok_non_matching_paren.pas
-mk_special_test ok_image ok_image.pas
-mk_special_test ok_include --introduction=ok_include_intro.txt ok_include.pas
-mk_special_test ok_class_var ok_class_var.pas
-mk_special_test error_line_number_3 error_line_number_3.pas
-mk_special_test ok_not_defined_omit ok_not_defined_omit.pas
-mk_special_test ok_abstract_sealed ok_abstract_sealed.pas
-mk_special_test ok_library ok_library.dpr
-mk_special_test ok_static_member ok_static_member.pas
-mk_special_test ok_strict_visibilities --visible-members protected,public,strictprotected,strictprivate ok_strict_visibilities.pas
-mk_special_test ok_weird_record ok_weird_record.dpr
-mk_special_test ok_include_quoted ok_include_quoted.pas
-mk_special_test ok_relative_include test_subdir/ok_relative_include_1.pas test_subdir/another_test_subdir/ok_relative_include_2.pas
-mk_special_test ok_list_item_set_number ok_list_item_set_number.pas
-mk_special_test ok_enum_field_var ok_enum_field_var.pas
-mk_special_test ok_back_comment_private ok_back_comment_private.pas
-mk_special_test ok_back_comment_class ok_back_comment_class.pas
-mk_special_test ok_excluded_unit ok_excluded_unit.pas
-mk_special_test ok_comment_over_uses_clause ok_comment_over_uses_clause.pas warning_back_comment_over_uses_clause.pas
-mk_special_test ok_interface_implicit ok_interface_implicit.pas --implicit-visibility=implicit
-mk_special_test ok_dot_unitname ok_dot_unitname.pas --write-uses-list
-mk_special_test ok_longcode_end_semicolon ok_longcode_end_semicolon.pas
-mk_special_test warning_desc_end warning_desc_end.pas
-mk_special_test ok_different_image_same_filename ok_different_image_same_filename_dir1/unit1.pas ok_different_image_same_filename_dir2/unit2.pas
-mk_special_test ok_enumeration_auto_abstract ok_enumeration_auto_abstract.pas --auto-abstract
-mk_special_test ok_ignore_leading ok_ignore_leading.pas --ignore-leading=*
-mk_special_test ok_ignore_leading_star ok_ignore_leading_star.pas --ignore-leading=* --staronly
-mk_special_test ok_ignore_leading_hash ok_ignore_leading_hash.pas --ignore-leading=#
-mk_special_test ok_ignore_leading_length2 ok_ignore_leading_length2.pas --ignore-leading=#?
-mk_special_test ok_packed_class_object ok_packed_class_object.pas
-mk_special_test ok_vorbisfile ok_vorbisfile.pas
-mk_special_test ok_procedural_const ok_procedural_const.pas
-mk_special_test ok_deprecated_const_string ok_deprecated_const_string.pas
-mk_special_test ok_external_class_hierarchy ok_external_class_hierarchy.pas --external-class-hierarchy=ok_external_class_hierarchy.txt
+mk_test ok_const_1st_comment_missing --marker=: "$SORT_OLD" ok_const_1st_comment_missing.pas
+mk_test ok_link_1_char --visible-members 'private,public,published' "$SORT_OLD" ok_link_1_char.pas
+mk_test ok_auto_abstract --auto-abstract "$SORT_OLD" ok_auto_abstract.pas
+mk_test warning_incorrect_tag_nesting "$SORT_OLD"  warning_incorrect_tag_nesting.pas
+mk_test ok_param_raises_returns_proctype "$SORT_OLD" ok_param_raises_returns_proctype.pas
+mk_test ok_no_sort '--sort=functions,non-record-fields,methods,properties' ok_no_sort.pas
+mk_test ok_sorting_all "$SORT_ALL" ok_sorting.pas
+mk_test ok_sorting_none --sort= ok_sorting.pas
+mk_test ok_introduction_conclusion ok_introduction_conclusion.pas --introduction=ok_introduction.txt --conclusion=ok_conclusion.txt
+mk_test ok_property_decl ok_property_decl.pas
+mk_test ok_multiple_vars ok_multiple_vars.pas
+mk_test ok_class_function ok_class_function.pas
+mk_test ok_latex_head --latex-head=ok_latex_head.tex ok_latex_head.pas
+mk_test ok_longcode_underscores ok_longcode_underscores.pas
+mk_test ok_longcode_comment ok_longcode_comment.pas
+mk_test ok_longcode_dash ok_longcode_dash.pas
+mk_test ok_longcode_special_chars ok_longcode_special_chars.pas
+mk_test error_introduction_twice_anchors_unit --introduction=error_introduction_twice_anchors.txt error_introduction_twice_anchors_unit.pas
+mk_test ok_longcode_float_hex ok_longcode_float_hex.pas
+mk_test ok_see_also ok_see_also.pas
+mk_test ok_implicit_visibility_public ok_implicit_visibility.pas
+mk_test ok_implicit_visibility_published --implicit-visibility=published ok_implicit_visibility.pas
+mk_test ok_implicit_visibility_implicit_yes --implicit-visibility=implicit --visible-members=public,implicit ok_implicit_visibility.pas
+mk_test ok_implicit_visibility_implicit_no  --implicit-visibility=implicit --visible-members=public          ok_implicit_visibility.pas
+mk_test ok_bold_italic ok_bold_italic.pas
+mk_test warning_inherited_test warning_inherited_test.pas
+mk_test ok_preformatted_test ok_preformatted_test.pas
+mk_test ok_dashes ok_dashes.pas
+mk_test ok_lists ok_lists.pas
+mk_test warning_lists warning_lists.pas
+mk_test ok_table ok_table.pas
+mk_test ok_table_nonlatex ok_table_nonlatex.pas
+mk_test warning_table warning_table.pas
+mk_test ok_macros ok_macros.pas
+mk_test error_macros error_macros.pas
+mk_test error_macros_recursive error_macros_recursive.pas
+mk_test ok_macros_off --no-macro ok_macros_off.pas
+mk_test ok_item_set_number ok_item_set_number.pas
+mk_test error_unexpected_eof error_unexpected_eof.pas
+mk_test error_unexpected_eof_2 error_unexpected_eof_2.pas
+mk_test error_expected_semicolon error_expected_semicolon.pas
+mk_test ok_record_case_parsing ok_record_case_parsing.pas
+mk_test ok_record_with_case ok_record_with_case.pas
+mk_test ok_multiple_fields ok_multiple_fields.pas
+mk_test ok_back_comment ok_back_comment.pas
+mk_test warning_back_comment warning_back_comment.pas
+mk_test ok_auto_link --auto-link ok_auto_link.pas
+mk_test ok_introduction_pre_link ok_introduction_pre_link_unit.pas --introduction=ok_introduction_pre_link.txt
+mk_test ok_table_of_contents ok_table_of_contents_unit.pas --introduction=ok_table_of_contents.txt
+mk_test warning_link_in_seealso warning_link_in_seealso.pas
+mk_test ok_caret_character ok_caret_character.pas
+mk_test ok_unit_uses_filename --write-uses-list ok_unit_uses_filename.pas
+mk_test ok_enum_explicit_assign ok_enum_explicit_assign.pas
+mk_test ok_if_directive ok_if_directive.pas
+mk_test ok_include_environment ok_include_environment.pas
+mk_test ok_enum_explicit_values ok_enum_explicit_values.pas
+mk_test ok_description_test --description ok_description_test.txt ok_description_test.pas
+mk_test ok_program --write-uses-list ok_program.pas
+mk_test ok_operator_test ok_operator_test.pas
+mk_test error_line_number_2 error_line_number_2.pas
+mk_test ok_dispid_method ok_dispid_method.pas
+mk_test ok_longcode_highlight ok_longcode_highlight.pas
+mk_test ok_non_matching_paren ok_non_matching_paren.pas
+mk_test ok_image ok_image.pas
+mk_test ok_include --introduction=ok_include_intro.txt ok_include.pas
+mk_test ok_class_var ok_class_var.pas
+mk_test error_line_number_3 error_line_number_3.pas
+mk_test ok_not_defined_omit ok_not_defined_omit.pas
+mk_test ok_abstract_sealed ok_abstract_sealed.pas
+mk_test ok_library ok_library.dpr
+mk_test ok_static_member ok_static_member.pas
+mk_test ok_strict_visibilities --visible-members protected,public,strictprotected,strictprivate ok_strict_visibilities.pas
+mk_test ok_weird_record ok_weird_record.dpr
+mk_test ok_include_quoted ok_include_quoted.pas
+mk_test ok_relative_include test_subdir/ok_relative_include_1.pas test_subdir/another_test_subdir/ok_relative_include_2.pas
+mk_test ok_list_item_set_number ok_list_item_set_number.pas
+mk_test ok_enum_field_var ok_enum_field_var.pas
+mk_test ok_back_comment_private ok_back_comment_private.pas
+mk_test ok_back_comment_class ok_back_comment_class.pas
+mk_test ok_excluded_unit ok_excluded_unit.pas
+mk_test ok_comment_over_uses_clause ok_comment_over_uses_clause.pas warning_back_comment_over_uses_clause.pas
+mk_test ok_interface_implicit ok_interface_implicit.pas --implicit-visibility=implicit
+mk_test ok_dot_unitname ok_dot_unitname.pas --write-uses-list
+mk_test ok_longcode_end_semicolon ok_longcode_end_semicolon.pas
+mk_test warning_desc_end warning_desc_end.pas
+mk_test ok_different_image_same_filename ok_different_image_same_filename_dir1/unit1.pas ok_different_image_same_filename_dir2/unit2.pas
+mk_test ok_enumeration_auto_abstract ok_enumeration_auto_abstract.pas --auto-abstract
+mk_test ok_ignore_leading ok_ignore_leading.pas --ignore-leading=*
+mk_test ok_ignore_leading_star ok_ignore_leading_star.pas --ignore-leading=* --staronly
+mk_test ok_ignore_leading_hash ok_ignore_leading_hash.pas --ignore-leading=#
+mk_test ok_ignore_leading_length2 ok_ignore_leading_length2.pas --ignore-leading=#?
+mk_test ok_packed_class_object ok_packed_class_object.pas
+mk_test ok_vorbisfile ok_vorbisfile.pas
+mk_test ok_procedural_const ok_procedural_const.pas
+mk_test ok_deprecated_const_string ok_deprecated_const_string.pas
+mk_test ok_external_class_hierarchy ok_external_class_hierarchy.pas --external-class-hierarchy=ok_external_class_hierarchy.txt
