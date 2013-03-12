c11a42a587d5ae67d93dd1f7846f26d32dcf3122
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-03-12 17:35:56 +0000

    file_to_pascal_string limits max string length now, avoids too long lines in source code, see https://sourceforge.net/p/pasdoc/bugs/82/ .
    git-svn-id: https://svn.code.sf.net/p/pasdoc/code/trunk@1342 e0417a13-a44b-4b77-80e5-317e04f1597d

--- source/tools/file_to_pascal_string.dpr
+++ source/tools/file_to_pascal_string.dpr
@@ -1,7 +1,7 @@
 { Reads text file $1 and writes Pascal source file $2,
   such that $2 file contains Pascal string that has the contents
-  of file $1. 
-  
+  of file $1.
+
   Useful if you want to "embed" some text file inside compiled
   Pascal program.
 
@@ -14,11 +14,18 @@ program file_to_pascal_string;
 {$ifdef FPC} {$mode objfpc} {$endif}
 {$H+}
 
-uses SysUtils;
+uses Math, SysUtils;
 
+const
+  { String constants are (at max) this long.
+    This avoids http://sourceforge.net/p/pasdoc/bugs/82/ problem
+    with Delphi XE compilation like
+    [DCC Fatal Error] jquery-1.7.1.min.js.inc(4): F2069 Line too long (more than 1023 characters) }
+  MaxStringLength = 1000;
 var
   Src, Dest: TextFile;
-  SrcFileName, DestFileName, S: string;
+  SrcFileName, DestFileName, S, Next: string;
+  LenProcessed, LenNext: Cardinal;
 begin
   SrcFileName := ParamStr(1);
   DestFileName := ParamStr(2);
@@ -36,10 +43,31 @@ begin
       while not Eof(Src) do
       begin
         Readln(Src, S);
-        S := StringReplace(S, '''', '''''', [rfReplaceAll]);
-        Writeln(Dest, '''' + S + ''' + LineEnding + ');
+
+        { Split S into MaxStringLength chunks }
+        if Length(S) <= MaxStringLength then
+        begin
+          { Special case when S already fits in MaxStringLength.
+            We could allow the loop below to handle any S with Length(S) <> 0,
+            the output would be correct, but for humans it looks better when
+            we output ' LineEnding + ' on the same line. }
+          S := StringReplace(S, '''', '''''', [rfReplaceAll]);
+          Writeln(Dest, '''' + S + ''' + LineEnding +');
+        end else
+        begin
+          LenProcessed := 0;
+          while LenProcessed < Length(S) do
+          begin
+            LenNext := Min(Length(S) - LenProcessed, MaxStringLength);
+            Next := Copy(S, LenProcessed + 1, LenNext);
+            Next := StringReplace(Next, '''', '''''', [rfReplaceAll]);
+            Writeln(Dest, '''' + Next + ''' + ');
+            LenProcessed := LenProcessed + LenNext;
+          end;
+          Writeln(Dest, 'LineEnding +');
+        end;
       end;
-      
+
       Writeln(Dest, '''''');
     finally CloseFile(Dest) end;
   finally CloseFile(Src) end;
