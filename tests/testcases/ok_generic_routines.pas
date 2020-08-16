62bf3352f7b74301f8f0dbddc00b242b6c8dba4c
Michalis Kamburelis <michalis.kambi@gmail.com>
2020-08-17 00:29:39 +0200

    Fix ok_generic_routines.pas compilation
    See https://github.com/pasdoc/pasdoc/pull/106

--- tests/testcases/ok_generic_routines.pas
+++ tests/testcases/ok_generic_routines.pas
@@ -1,25 +1,36 @@
+{$ifdef FPC} {$mode objfpc}{$H+} {$endif}
+
 unit ok_generic_routines;
 
 interface
 
 type
+  TTestClass = class
+  end;
 
-	TTestClass = class	
-	end;
-
-generic function DoSomething<T>(Input: String): T;
-generic procedure DoSomething2<T>(Input: T);
+generic function DoSomething<T>(const Caption: String; const Input: T): T;
+generic procedure DoSomething2<T>(const Input: T);
 
 implementation
 
-generic function DoSomething<T>(Input: String): T;
+generic function DoSomething<T>(const Caption: String; const Input: T): T;
 begin
   Result := Input;
-end
+end;
 
-generic procedure DoSomething2<T>(Input: T);
+generic procedure DoSomething2<T>(const Input: T);
 begin
 
+end;
+
+procedure Foo;
+var
+  X: Integer;
+begin
+  X := specialize DoSomething<Integer>('asdads', 123);
+  Writeln(X);
+
+  specialize DoSomething2<Integer>(456);
 end;
 
 end.
