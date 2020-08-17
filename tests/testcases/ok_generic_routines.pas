89b97fea2ad641105d976ea9848849ef74ab3dc6
Imants Gulbis <imants.gulbis@gmail.com>
2020-08-17 19:23:46 +0300

    Added support for generics in simple classes and in implementation section

--- tests/testcases/ok_generic_routines.pas
+++ tests/testcases/ok_generic_routines.pas
@@ -8,29 +8,50 @@ type
   TTestClass = class
   end;
 
-generic function DoSomething<T>(const Caption: String; const Input: T): T;
-generic procedure DoSomething2<T>(const Input: T);
+  { TTestClass }
+
+  TTestClass = class
+  public
+    (* Test documentation for class function DoSomething *)
+    generic function DoSomething<T>(Input: String): T;
+    (* Test documentation for class procedure DoSomething2 *)
+    generic procedure DoSomething2<T>(Input: T);
+  end;
+
+(* Test documentation for function DoSomething *)
+generic function DoSomething<T>(Input: String): T;
+(* Test documentation for procedure DoSomething2 *)
+generic procedure DoSomething2<T>(Input: T);
 
 implementation
 
-generic function DoSomething<T>(const Caption: String; const Input: T): T;
+(* Test documentation for function DoSomething implementation *)
+generic function DoSomething<T>(Input: String): T;
 begin
   Result := Input;
 end;
 
-generic procedure DoSomething2<T>(const Input: T);
-begin
-
-end;
-
-procedure Foo;
-var
-  X: Integer;
+(* Test documentation for procedure DoSomething2 implementation *)
+generic procedure DoSomething2<T>(Input: T);
 begin
   X := specialize DoSomething<Integer>('asdads', 123);
   Writeln(X);
 
   specialize DoSomething2<Integer>(456);
+end;
+
+{ TTestClass }
+
+(* Test documentation for class function DoSomething implementation *)
+generic function TTestClass.DoSomething<T>(Input: String): T;
+begin
+  Result := Input;
+end;
+
+(* Test documentation for class procedure DoSomething2 implementation *)
+generic procedure TTestClass.DoSomething2<T>(Input: T);
+begin
+
 end;
 
 end.
