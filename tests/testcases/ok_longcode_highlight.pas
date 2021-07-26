5c9a9b327e07e5a7493492126e1193780d6283fa
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-07-26 02:53:02 +0200

    Highlighting numbers in @longCode test

--- tests/testcases/ok_longcode_highlight.pas
+++ tests/testcases/ok_longcode_highlight.pas
@@ -9,7 +9,18 @@
     procedure WMSysCmd(var Msg: TWMSysCommand); message WM_SYSCOMMAND;
   ...
   end;
-  #) }
+  #)
+
+  See https://github.com/pasdoc/pasdoc/issues/123 :
+
+  @longcode(#
+  Somethins.Name:=1;
+  I:=123;
+  R:=0.001;
+  R:=1E10;
+  R:=R*0.99 - 9E-4;
+  #)
+}
 unit ok_longcode_highlight;
 
 interface
