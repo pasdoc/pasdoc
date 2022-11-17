6a98aba8d20262af13b694245cdb4580d2b98486
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-11-17 03:22:31 +0100

    More tests in ok_markdown_links.pas

--- tests/testcases/ok_markdown_links.pas
+++ tests/testcases/ok_markdown_links.pas
@@ -3,15 +3,19 @@
   Link to proc inside this unit with description: [Test Procedure](#MyProc)
 
   Not links:
-  
+
   - \[Test Procedure](#MyProc)
-  
+
 }
 unit ok_markdown_links;
 
 interface
 
-{ Link to [Self](#MyProc), and a [Second one](#ok_markdown_links.MyProc) }
+{ Link to [Self](#MyProc), and a [Second one](#ok_markdown_links.MyProc).
+  Link without explicit title [ok_markdown_links].
+
+  Not links to \[Self](#MyProc), and a \[Second one](#ok_markdown_links.MyProc).
+  Not a link without explicit title \[ok_markdown_links]. }
 procedure MyProc;
 
 implementation
