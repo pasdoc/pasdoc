52c2f62bbd9422fa9f310f293565892a3fdc8006
Elliot Hillary <ehillary@integradev.com.au>
2022-12-07 14:50:44 +1100

    Regenerate test case output

---
+++ tests/testcases_output/php/ok_parameter_default_values/docs.php
@@ -0,0 +1,7 @@
+<?php
+global $pasdoc;
+$pasdoc = array(
+  'ok_parameter_default_values' => array('html_filename' => 'ok_parameter_default_values.html', 'type' => 'unit'),
+  'Foo' => array('html_filename' => 'ok_parameter_default_values.html#Foo-Integer-Integer-Integer-', 'type' => 'procedure'),
+  'Bar' => array('html_filename' => 'ok_parameter_default_values.html#Bar-Integer-Integer-Integer-string-', 'type' => 'procedure'),
+);
