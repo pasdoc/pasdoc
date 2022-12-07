814587c52399d7aa0c9054f45293e64489c2a610
Elliot Hillary <ehillary@integradev.com.au>
2022-12-07 13:19:24 +1100

    Add tests for parameter parsing and link tag overloads

---
+++ tests/testcases/ok_parameter_default_values.pas
@@ -0,0 +1,10 @@
+unit ok_parameter_default_values;
+
+interface
+
+procedure Foo(A: Integer; B: Integer = 3; C: Integer = 5);
+procedure Bar(A, B: Integer; C: Integer = 3; D: string = '');
+
+implementation
+
+end.
