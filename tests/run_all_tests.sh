a1fc28b8476d38b66a09fea56c0dffd1aaf4b598
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-01-16 22:44:20 +0100

    Allow to $MAKE to tests script

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-set -eu
+set -euo pipefail
 
+# ----------------------------------------------------------------------------
 # Run all PasDoc tests.
+#
+# Uses (subset of) http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# ----------------------------------------------------------------------------
 
-make clean
+# Set $MAKE, unless already set.
+# This allows parent process to pass MAKE value to this script.
+# We expect GNU make (this option allows to set it,
+# e.g. on Windows or FreeBSD where other "make" may be on $PATH).
+if [ "${MAKE:-}" = '' ]; then
+  MAKE='make'
+fi
+
+$MAKE clean
 
 # fpcunit tests --------------------------------------------------------------
 
 pushd fpcunit
-make
+$MAKE
 popd
 
 # build pasdoc --------------------------------------------------------------
 
 pushd ../
-make
+$MAKE
 popd
 
 # ----------------------------------------------------------------------------
