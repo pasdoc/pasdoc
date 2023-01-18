da237b37f82a1f3c8f80b1ec8d03b93c4102e818
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-01-18 02:48:00 +0100

    Update testcase output after previous commit
    See https://github.com/pasdoc/pasdoc/issues/174

--- tests/testcases_output/php/ok_property_decl/docs.php
+++ tests/testcases_output/php/ok_property_decl/docs.php
@@ -4,4 +4,11 @@ $pasdoc = array(
   'ok_property_decl' => array('html_filename' => 'ok_property_decl.html', 'type' => 'unit'),
   'TMyClass' => array('html_filename' => 'ok_property_decl.TMyClass.html', 'type' => 'class'),
   'TMyClass.MyProperty' => array('html_filename' => 'ok_property_decl.TMyClass.html#MyProperty', 'type' => 'property'),
+  'TMyClass.SelLength' => array('html_filename' => 'ok_property_decl.TMyClass.html#SelLength', 'type' => 'property'),
+  'TMyClass.SelText' => array('html_filename' => 'ok_property_decl.TMyClass.html#SelText', 'type' => 'property'),
+  'TMyClass.KeyboardType' => array('html_filename' => 'ok_property_decl.TMyClass.html#KeyboardType', 'type' => 'property'),
+  'TMyClass.MyPropertyNoDefault' => array('html_filename' => 'ok_property_decl.TMyClass.html#MyPropertyNoDefault', 'type' => 'property'),
+  'TMyClass.MyPropertyDefaultExpression' => array('html_filename' => 'ok_property_decl.TMyClass.html#MyPropertyDefaultExpression', 'type' => 'property'),
+  'TMyClass.MyPropertyStored' => array('html_filename' => 'ok_property_decl.TMyClass.html#MyPropertyStored', 'type' => 'property'),
+  'TMyClass.MyPropertyStored2' => array('html_filename' => 'ok_property_decl.TMyClass.html#MyPropertyStored2', 'type' => 'property'),
 );
