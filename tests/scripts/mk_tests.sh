469cb03a4190b43fdcde71c743502f79cd7d374d
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-03-28 01:55:38 +0200

    Temp remove --exclude-generator -- debugging GH actions fail

--- tests/scripts/mk_tests.sh
+++ tests/scripts/mk_tests.sh
@@ -59,7 +59,7 @@ mk_test ()
   # of parsing all units and that it won't change output on files with empty or
   # fully private impl sections
   run_echo "$PASDOC_OUTPUT_FILENAME" \
-    "${PASDOC_BIN}" --format "$FORMAT" --exclude-generator \
+    "${PASDOC_BIN}" --format "$FORMAT" \
     --implementation-comments=join --output="$OUTPUT_PATH" "$@"
 }
 
