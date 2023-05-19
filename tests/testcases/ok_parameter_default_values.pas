a03490aa4d4486168e95e2ccdb0ec58d7e3896e2
Elliot Hillary <ehillary@integradev.com.au>
2023-05-19 15:48:03 +1000

    Add Boolean default param function to test

--- tests/testcases/ok_parameter_default_values.pas
+++ tests/testcases/ok_parameter_default_values.pas
@@ -4,6 +4,7 @@ interface
 
 procedure Foo(A: Integer; B: Integer = 3; C: Integer = 5);
 procedure Bar(A, B: Integer; C: Integer = 3; D: string = '');
+function Baz(A: Boolean = True): string;
 
 implementation
 
