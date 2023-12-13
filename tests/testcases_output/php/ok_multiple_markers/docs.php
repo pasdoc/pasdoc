f635022ac74a65a4f08acd3efe59b6ebcebd9505
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-12-13 23:56:26 +0100

    Testcase that multiple markers work
    See https://github.com/pasdoc/pasdoc/issues/192

---
+++ tests/testcases_output/php/ok_multiple_markers/docs.php
@@ -0,0 +1,7 @@
+<?php
+global $pasdoc;
+$pasdoc = array(
+  'ok_multiple_markers' => array('html_filename' => 'ok_multiple_markers.html', 'type' => 'unit'),
+  'Marker1' => array('html_filename' => 'ok_multiple_markers.html#Marker1', 'type' => 'variable'),
+  'Marker2' => array('html_filename' => 'ok_multiple_markers.html#Marker2', 'type' => 'variable'),
+);
