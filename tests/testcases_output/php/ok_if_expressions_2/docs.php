e0ec5de5e38ec6e395dca4b8ac62a3c511b39f06
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-03-13 11:08:04 +0100

    Add ok_if_expressions_2 testcase, along with correct output ("make tests")

---
+++ tests/testcases_output/php/ok_if_expressions_2/docs.php
@@ -0,0 +1,20 @@
+<?php
+global $pasdoc;
+$pasdoc = array(
+  'ok_if_expressions_2' => array('html_filename' => 'ok_if_expressions_2.html', 'type' => 'unit'),
+  '_TrueXorTrueIsFalse' => array('html_filename' => 'ok_if_expressions_2.html#_TrueXorTrueIsFalse', 'type' => 'constant'),
+  '_3Plus1Is4' => array('html_filename' => 'ok_if_expressions_2.html#_3Plus1Is4', 'type' => 'constant'),
+  '_APlusBIsAB' => array('html_filename' => 'ok_if_expressions_2.html#_APlusBIsAB', 'type' => 'constant'),
+  '_3Plus2Times2Is7' => array('html_filename' => 'ok_if_expressions_2.html#_3Plus2Times2Is7', 'type' => 'constant'),
+  '_3Minus1IsNot4' => array('html_filename' => 'ok_if_expressions_2.html#_3Minus1IsNot4', 'type' => 'constant'),
+  '_3Minus2IsTrue' => array('html_filename' => 'ok_if_expressions_2.html#_3Minus2IsTrue', 'type' => 'constant'),
+  '_Not3Plus5IsTrue' => array('html_filename' => 'ok_if_expressions_2.html#_Not3Plus5IsTrue', 'type' => 'constant'),
+  '_Not1Is0' => array('html_filename' => 'ok_if_expressions_2.html#_Not1Is0', 'type' => 'constant'),
+  '_Not0Is1' => array('html_filename' => 'ok_if_expressions_2.html#_Not0Is1', 'type' => 'constant'),
+  '_2Or4Is6' => array('html_filename' => 'ok_if_expressions_2.html#_2Or4Is6', 'type' => 'constant'),
+  '_6And4Is4' => array('html_filename' => 'ok_if_expressions_2.html#_6And4Is4', 'type' => 'constant'),
+  '_2And6Is2' => array('html_filename' => 'ok_if_expressions_2.html#_2And6Is2', 'type' => 'constant'),
+  'FPCVersionAtLeast322' => array('html_filename' => 'ok_if_expressions_2.html#FPCVersionAtLeast322', 'type' => 'constant'),
+  'FPCVersionAtLeast422' => array('html_filename' => 'ok_if_expressions_2.html#FPCVersionAtLeast422', 'type' => 'constant'),
+  'MyConst' => array('html_filename' => 'ok_if_expressions_2.html#MyConst', 'type' => 'constant'),
+);
