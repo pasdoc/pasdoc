acba08e4922559e073b2b17699ea831d5dc4f6d9
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-07-09 13:55:28 +0200

    Fix begin / end indentation, we prefer to make them aligned

--- source/component/PasDoc_Gen.pas
+++ source/component/PasDoc_Gen.pas
@@ -2684,16 +2684,19 @@ begin
       else Assert(false, 'LinkLook = ??');
     end;
   end
-  else begin
-    if (WarningIfLinkNotFound = lnfWarnIfNotInternal)
-      and (FExternalClassHierarchy.IndexOfName(S) <> -1) then begin
+  else
+  begin
+    if (WarningIfLinkNotFound = lnfWarnIfNotInternal) and
+       (FExternalClassHierarchy.IndexOfName(S) <> -1) then
+    begin
       DoMessage(
         6,
         pmtInformation,
         'Link "%s" resolved as reference to external class hierarchy (from description of "%s")',
         [S, Item.QualifiedName]);
-    end
-    else if (WarningIfLinkNotFound <> lnfIgnore) then begin
+    end else
+    if (WarningIfLinkNotFound <> lnfIgnore) then
+    begin
       DoMessage(1, pmtWarning, 'Could not resolve link "%s" (from description of "%s")',
         [S, Item.QualifiedName]);
     end;
