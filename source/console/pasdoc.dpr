1c2bf522824e878c87e7035465d5f48009acc8ec
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-03-04 04:00:53 +0100

    Add all units to Delphi DPROJ

--- source/console/pasdoc.dpr
+++ source/console/pasdoc.dpr
@@ -36,7 +36,36 @@ program pasdoc;
 {$endif}
 
 uses
-  PasDoc_Main;
+  PasDoc_Main in 'PasDoc_Main.pas',
+  PasDoc_Aspell in '..\component\PasDoc_Aspell.pas',
+  PasDoc_Base in '..\component\PasDoc_Base.pas',
+  PasDoc_Gen in '..\component\PasDoc_Gen.pas',
+  PasDoc_GenHtml in '..\component\PasDoc_GenHtml.pas',
+  PasDoc_GenHtmlHelp in '..\component\PasDoc_GenHtmlHelp.pas',
+  PasDoc_GenLatex in '..\component\PasDoc_GenLatex.pas',
+  PasDoc_GenPHP in '..\component\PasDoc_GenPHP.pas',
+  PasDoc_GenSimpleXML in '..\component\PasDoc_GenSimpleXML.pas',
+  PasDoc_Hashes in '..\component\PasDoc_Hashes.pas',
+  PasDoc_HierarchyTree in '..\component\PasDoc_HierarchyTree.pas',
+  PasDoc_Items in '..\component\PasDoc_Items.pas',
+  PasDoc_Languages in '..\component\PasDoc_Languages.pas',
+  PasDoc_OptionParser in '..\component\PasDoc_OptionParser.pas',
+  PasDoc_Parser in '..\component\PasDoc_Parser.pas',
+  PasDoc_ProcessLineTalk in '..\component\PasDoc_ProcessLineTalk.pas',
+  PasDoc_Reg in '..\component\PasDoc_Reg.pas',
+  PasDoc_Scanner in '..\component\PasDoc_Scanner.pas',
+  PasDoc_Serialize in '..\component\PasDoc_Serialize.pas',
+  PasDoc_SortSettings in '..\component\PasDoc_SortSettings.pas',
+  PasDoc_StreamUtils in '..\component\PasDoc_StreamUtils.pas',
+  PasDoc_StringPairVector in '..\component\PasDoc_StringPairVector.pas',
+  PasDoc_StringVector in '..\component\PasDoc_StringVector.pas',
+  PasDoc_TagManager in '..\component\PasDoc_TagManager.pas',
+  PasDoc_Tokenizer in '..\component\PasDoc_Tokenizer.pas',
+  PasDoc_Types in '..\component\PasDoc_Types.pas',
+  PasDoc_Utils in '..\component\PasDoc_Utils.pas',
+  PasDoc_Versions in '..\component\PasDoc_Versions.pas',
+  PasDoc_Tipue in '..\component\tipue\PasDoc_Tipue.pas';
+
 begin
   Main;
 end.
