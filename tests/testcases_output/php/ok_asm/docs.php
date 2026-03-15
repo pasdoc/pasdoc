ad925b86822d701a7f4bb588e046614ffb8f75d1
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-03-16 00:29:27 +0100

    ok_asm test results, see https://github.com/pasdoc/pasdoc/issues/69

---
+++ tests/testcases_output/php/ok_asm/docs.php
@@ -0,0 +1,6 @@
+<?php
+global $pasdoc;
+$pasdoc = array(
+  'ok_asm' => array('html_filename' => 'ok_asm.html', 'type' => 'unit'),
+  'Foo' => array('html_filename' => 'ok_asm.html#Foo', 'type' => 'procedure'),
+);
