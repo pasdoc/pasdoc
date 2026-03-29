62ecafa5e01d1445b101532be1235bfdf0d3d501
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-03-29 16:05:05 +0200

    Update correct output after ok_helpinsight_comments.pas change

--- tests/testcases_output/php/ok_helpinsight_comments/docs.php
+++ tests/testcases_output/php/ok_helpinsight_comments/docs.php
@@ -3,4 +3,7 @@ global $pasdoc;
 $pasdoc = array(
   'ok_helpinsight_comments' => array('html_filename' => 'ok_helpinsight_comments.html', 'type' => 'unit'),
   'Parse' => array('html_filename' => 'ok_helpinsight_comments.html#Parse-string-', 'type' => 'procedure'),
+  'PascalCodeSample' => array('html_filename' => 'ok_helpinsight_comments.html#PascalCodeSample', 'type' => 'procedure'),
+  'MultilineCodeSample' => array('html_filename' => 'ok_helpinsight_comments.html#MultilineCodeSample', 'type' => 'procedure'),
+  'MultilineSummaryExample' => array('html_filename' => 'ok_helpinsight_comments.html#MultilineSummaryExample', 'type' => 'procedure'),
 );
