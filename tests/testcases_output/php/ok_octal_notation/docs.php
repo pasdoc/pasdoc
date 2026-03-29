18bc3d97452452816ced9ce470f13c9899b368fb
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-03-29 18:44:58 +0200

    Correct tests output for ok_octal_notation

---
+++ tests/testcases_output/php/ok_octal_notation/docs.php
@@ -0,0 +1,10 @@
+<?php
+global $pasdoc;
+$pasdoc = array(
+  'ok_octal_notation' => array('html_filename' => 'ok_octal_notation.html', 'type' => 'unit'),
+  'S_ISUID' => array('html_filename' => 'ok_octal_notation.html#S_ISUID', 'type' => 'constant'),
+  'S_ISGID' => array('html_filename' => 'ok_octal_notation.html#S_ISGID', 'type' => 'constant'),
+  'S_ISVTX' => array('html_filename' => 'ok_octal_notation.html#S_ISVTX', 'type' => 'constant'),
+  'IN_CLOEXEC' => array('html_filename' => 'ok_octal_notation.html#IN_CLOEXEC', 'type' => 'constant'),
+  'IN_NONBLOCK' => array('html_filename' => 'ok_octal_notation.html#IN_NONBLOCK', 'type' => 'constant'),
+);
