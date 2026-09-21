d651a30d472b93189fa3ea002af7f36044ce10d3
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-09-21 13:32:31 +0200

    ok_winapi testcase correct result

---
+++ tests/testcases_output/php/ok_winapi/docs.php
@@ -0,0 +1,7 @@
+<?php
+global $pasdoc;
+$pasdoc = array(
+  'ok_winapi' => array('html_filename' => 'ok_winapi.html', 'type' => 'unit'),
+  'SslChangeNotify' => array('html_filename' => 'ok_winapi.html#SslChangeNotify-THandle-Integer-', 'type' => 'function'),
+  'SslChangeNotify2' => array('html_filename' => 'ok_winapi.html#SslChangeNotify2-THandle-Integer-', 'type' => 'function'),
+);
